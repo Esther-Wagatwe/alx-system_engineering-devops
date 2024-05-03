06ad11e05a4f1a7c304d0345ad6e5dba2b8f7383
Esther-Wagatwe <estherwagatwe@gmail.com>
2024-05-03 23:01:18 +0300

    Install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
 #Installs flask from pip3, version 2.1.0
 
-exec { 'install_flask':
-    command => '/usr/bin/pip3 install flask==2.1.0',
-    unless  => '/usr/bin/pip3 show flask | grep Version | grep -q 2.1.0',
+package { 'flask':
+    ensure   => '2.1.0',
+    provider => 'pip3'
 }
