83af71d4f29d2b34e2ef39fddf2f8231ff45348c
Esther-Wagatwe <estherwagatwe@gmail.com>
2024-05-04 08:51:46 +0300

    Install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,12 @@
 #!/usr/bin/pup
 #Install flask from pip3, version 2.1.0
 
-package {'flask':
+package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3'
+}
+
+package { 'werkzeug':
+    ensure   => '2.1.1',
+    provider => 'pip3'
 }
