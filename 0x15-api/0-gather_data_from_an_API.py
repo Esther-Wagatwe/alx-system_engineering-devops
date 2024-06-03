c3b9538ec1403d65845a21df780b9151938d297d
Esther-Wagatwe <estherwagatwe@gmail.com>
2024-06-03 13:55:35 +0300

    Pycodestyle correction

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""Module that uses REST API, for a given employee ID, returns information about his/her TODO list progress."""
+"""Module that uses REST API, for a given employee ID, returns information
+about his/her TODO list progress."""
 import requests
 import sys
 
@@ -10,7 +11,7 @@ if __name__ == "__main__":
     user_response = requests.get(user_url)
     user_name = user_response.json().get('name')
 
-    todos_url = f'https://jsonplaceholder.typicode.com/todos?userId={user_ID}'    
+    todos_url = f'https://jsonplaceholder.typicode.com/todos?userId={user_ID}'
     todos_response = requests.get(todos_url)
     tasks = todos_response.json()
     total_tasks_done = 0
@@ -21,7 +22,8 @@ if __name__ == "__main__":
             done_tasks.append(task)
             total_tasks_done += 1
 
-    print("Employee {} is done with tasks({}/{})".format(user_name, total_tasks_done, len(tasks)))
+    print("Employee {} is done with tasks({}/{})"
+          .format(user_name, total_tasks_done, len(tasks)))
 
     for task in done_tasks:
         print("\t {}".format(task.get('title')))
