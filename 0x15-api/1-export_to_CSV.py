3d4e59fc55c8b5de5343d1ed8f579c08e36074a4
Esther-Wagatwe <estherwagatwe@gmail.com>
2024-06-03 15:24:50 +0300

    Export to CSV

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     user_ID = sys.argv[1]
     user_url = f'https://jsonplaceholder.typicode.com/users/{user_ID}'
     user_response = requests.get(user_url)
-    user_name = user_response.json().get('name')
+    user_name = user_response.json().get('username')
 
     todos_url = f'https://jsonplaceholder.typicode.com/todos?userId={user_ID}'
     todos_response = requests.get(todos_url)
