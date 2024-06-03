0f7edb4d96bc64ccc0a7d6d8d255987c1abb10e0
Esther-Wagatwe <estherwagatwe@gmail.com>
2024-06-03 15:00:07 +0300

    Export to JSON

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,30 +1,38 @@
 #!/usr/bin/python3
 """Module that uses REST API, for a given employee ID, returns information
 about his/her TODO list progress."""
-import requests
-import sys
+from requests import get
+from sys import argv
+
+def todo(employer_id):
+    """ Send request for employee's
+        to do list to API
+    """
+    total_tasks = 0
+    completed_tasks = 0
+    url_user = 'https://jsonplaceholder.typicode.com/users/'
+    url_todo = 'https://jsonplaceholder.typicode.com/todos/'
+
+    # check if user exists
+    user = get(url_user + employer_id).json().get('name')
+
+    if user:
+        params = {'userId': employer_id}
+        #  get all tasks
+        tasks = get(url_todo, params=params).json()
+        if tasks:
+            total_tasks = len(tasks)
+            #  get number of completed tasks
+            params.update({'completed': 'true'})
+            completed = len(get(url_todo, params=params).json())
+
+        print("Employee {} is done with tasks({}/{}):".format(
+            user, completed_tasks, total_tasks))
+        for task in tasks:
+            if task.get('completed') is True:
+                print("\t {}".format(task.get('title')))
 
 
 if __name__ == '__main__':
-    employee_Id = sys.argv[1]
-    url = f'https://jsonplaceholder.typicode.com/users/{employee_Id}'
-
-    response = requests.get(url)
-    employee_name = response.json().get('name')
-
-    todo_Url = url + "/todos"
-    response = requests.get(todo_Url)
-    tasks = response.json()
-    completed_tasks = 0
-    done_tasks = []
-
-    for task in tasks:
-        if task.get('completed'):
-            done_tasks.append(task)
-            completed_tasks += 1
-
-    print("Employee {} is done with tasks({}/{}):"
-          .format(employee_name, completed_tasks, len(tasks)))
-
-    for task in done_tasks:
-        print("\t {}".format(task.get('title')))
+    if len(argv) > 1:
+        todo(argv[1])
