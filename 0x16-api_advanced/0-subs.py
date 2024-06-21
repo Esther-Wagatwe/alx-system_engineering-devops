6b192f743ade1b147a614407cda3fd2fe7074ef3
Esther-Wagatwe <estherwagatwe@gmail.com>
2024-06-21 05:54:38 +0300

    Debugging

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -14,7 +14,7 @@ def number_of_subscribers(subreddit):
         'Mozilla/5.0 (Windows; U; Windows NT 5.1; de; rv:1.9.2.3) \
         Gecko/20100401 Firefox/3.6.3 (FM Scene 4.6.1)'
     }
-    results = requests.get(
+    results = get(
         url,
         headers=headers,
         allow_redirects=False
