e0c2fdb1a2e02e021f955b7df37e6a9dd967e177
Esther-Wagatwe <estherwagatwe@gmail.com>
2024-06-21 05:57:28 +0300

    Debugging

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -16,7 +16,7 @@ def top_ten(subreddit):
         'Mozilla/5.0 (Windows; U; Windows NT 5.1; de; rv:1.9.2.3) \
         Gecko/20100401 Firefox/3.6.3 (FM Scene 4.6.1)'
     }
-    response = requests.get(
+    response = get(
         url,
         headers=headers,
         allow_redirects=False
