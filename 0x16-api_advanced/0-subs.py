3dcd703470eb57cf2bb98ce0b5564baf6fd5482f
Esther-Wagatwe <estherwagatwe@gmail.com>
2024-06-21 05:49:03 +0300

    Corrections

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,7 +7,11 @@ def number_of_subscribers(subreddit):
     """queries the Reddit API
     returns the number of subscribers for a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "My-User-Agent"}
+    headers = {
+        'User-Agent':
+        'Mozilla/5.0 (Windows; U; Windows NT 5.1; de; rv:1.9.2.3) \
+        Gecko/20100401 Firefox/3.6.3 (FM Scene 4.6.1)'
+    }
     results = get(
         url,
         headers=headers,
