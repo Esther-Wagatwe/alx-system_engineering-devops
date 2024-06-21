45a6bb828d240b5ca41a507c7a6569e5398bcfd9
Esther-Wagatwe <estherwagatwe@gmail.com>
2024-06-21 05:52:25 +0300

    Corrections

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,13 +6,15 @@ from requests import get
 def number_of_subscribers(subreddit):
     """queries the Reddit API
     returns the number of subscribers for a given subreddit."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+       base_url = 'https://www.reddit.com/r/'
+
+    url = '{}{}/about.json'.format(base_url, subreddit)
     headers = {
         'User-Agent':
         'Mozilla/5.0 (Windows; U; Windows NT 5.1; de; rv:1.9.2.3) \
         Gecko/20100401 Firefox/3.6.3 (FM Scene 4.6.1)'
     }
-    results = get(
+    results = requests.get(
         url,
         headers=headers,
         allow_redirects=False
