cec3d7d30f532cfe520209231110ff4945115f10
Esther-Wagatwe <estherwagatwe@gmail.com>
2024-06-21 05:56:13 +0300

    Debugging

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -6,11 +6,23 @@ from requests import get
 def top_ten(subreddit):
     """queries the Reddit API
     returns the top 10 hot posts"""
-    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
-    headers = {"User-Agent": "My-User-Agent"}
-    results = get(url, headers, allow_redirects=False)
-    if results.status_code == 200:
-        for post in results.json()['data']['children'][0:10]:
+    base_url = 'https://www.reddit.com'
+    sort = 'top'
+    limit = 10
+    url = '{}/r/{}/.json?sort={}&limit={}'.format(
+        base_url, subreddit, sort, limit)
+    headers = {
+        'User-Agent':
+        'Mozilla/5.0 (Windows; U; Windows NT 5.1; de; rv:1.9.2.3) \
+        Gecko/20100401 Firefox/3.6.3 (FM Scene 4.6.1)'
+    }
+    response = requests.get(
+        url,
+        headers=headers,
+        allow_redirects=False
+    )
+    if response.status_code == 200:
+        for post in response.json()['data']['children'][0:10]:
             print(post['data']['title'])
     else:
         print(None)
